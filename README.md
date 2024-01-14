ebc6588a1d3a88984619983202c5961d71625f70
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-01-14 14:28:26 +0100

    chore: improved target folder

--- README.md
+++ README.md
@@ -3,13 +3,9 @@
 This project allows you to create a pechakucha presentation from a list of pictures in a folder, sorted in lexicographic order.
 
 ```sh
-pechakucha-gen ./your_folder
+pechakucha-gen <your_folder> <output_folder>
 ```
 
-**By default** the project deletes everything in the `output` folder where you run the CLI.
+**By default** the project deletes everything in the `output_folder` folder you provide to the CLI.
 
-To run you presentation, you can use;
-
-```sh
-pnpm dlx serve output
-```
+To run you presentation, you can open the file pechakucha.html. Beware, it starts as soon as you open the file.
--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,36 @@ use pechakucha_gen::generate_presentation;
 /// toutes les 20 secondes. Bon courage !
 fn main() -> Result<(), std::io::Error> {
     // Obtenir le chemin vers le dossier
-    let path_folder = get_folder_path();
+    let (path_folder, output_folder) = get_folder_path();
 
     // Créer un html contenant notre page web
     let (output, paths) = generate_presentation(&path_folder)?;
-    write_presentation(&output, paths)?;
+    write_presentation(&output, paths, output_folder)?;
 
     Ok(())
 }
 
-fn write_presentation(output: &str, paths: Vec<PathBuf>) -> Result<(), std::io::Error> {
-    let create_dir = fs::create_dir("output");
+fn write_presentation(
+    output: &str,
+    paths: Vec<PathBuf>,
+    output_folder: PathBuf,
+) -> Result<(), std::io::Error> {
+    let path = output_folder
+        .to_str()
+        .expect("Something went very wrong when unwrapping the output folder to a path.");
+    let create_dir = fs::create_dir(path);
     if let Err(e) = create_dir {
         match e.kind() {
             std::io::ErrorKind::AlreadyExists => {
-                fs::remove_dir_all("output")?;
-                fs::create_dir("output")?;
+                fs::remove_dir_all(path)?;
+                fs::create_dir(path)?;
             }
             _ => {
                 return Err(e);
             }
         }
     }
-    let mut presentation_file = File::create("output/pechakucha.html")?;
+    let mut presentation_file = File::create(format!("{path}/pechakucha.html"))?;
     presentation_file.write(output.as_bytes())?;
 
     // Copy all images in folder
@@ -46,13 +53,13 @@ fn write_presentation(output: &str, paths: Vec<PathBuf>) -> Result<(), std::io::
             .expect("Ne peut pas fonctionner sans filename")
             .to_str()
             .unwrap();
-        fs::copy(image_path, format!("output/{}", file_name))?;
+        fs::copy(image_path, format!("{path}/{}", file_name))?;
     }
     println!("Created presentation pechakucha.html");
     Ok(())
 }
 
-fn get_folder_path() -> PathBuf {
+fn get_folder_path() -> (PathBuf, PathBuf) {
     let mut args = env::args();
     args.next();
     // Skip path
@@ -60,6 +67,9 @@ fn get_folder_path() -> PathBuf {
         eprintln!("Missing argument: needs a path to a folder.");
         process::exit(1);
     });
+
     let path_folder = PathBuf::from(path_folder);
-    path_folder
+    let output_folder = args.next().unwrap_or(String::from("./output"));
+    let output_folder = PathBuf::from(output_folder);
+    (path_folder, output_folder)
 }
